d4781f3458d816aa87316d878c99f7497b254a0a
RodrigoTz0 <rodrigofragaebling@gmail.com>
2024-12-06 17:45:19 -0300

    add human choice

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-console.log("Hello World")
+console.log("Fist one is your answer and the second one is the machine")
+let getHumanChoice = prompt("Choose your option")
+if (getHumanChoice.toLowerCase() === "rock") {
+    console.log("Rock");
+} else if (getHumanChoice.toLowerCase() === "paper") {
+    console.log("Paper");
+} else if (getHumanChoice.toLowerCase() === "scissors") {
+    console.log("Scissors");
+} else {
+    console.log("Please select a valid option");
+}
+
 let randomNumber = Math.floor(Math.random() * 3) + 1;
 function getComputerChoice (randomNumber) {
     if (randomNumber === 1) {
